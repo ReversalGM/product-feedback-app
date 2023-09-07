90226cbafa44528a1668ea9e26e2ae9704b641e3
ReversalGM <zhouhyan.steven@gmail.com>
2023-09-07 19:34:03 -0400

    FeedbackCard reads data

--- src/FeedbackCard/FeedbackCard.jsx
+++ src/FeedbackCard/FeedbackCard.jsx
@@ -1,31 +1,42 @@
 import "./FeedbackCard.css"
-
-export function FeedbackCard(props) {
+import { useState } from "react"
+export function FeedbackCard({
+  id,
+  title,
+  category,
+  upvotes,
+  status,
+  description,
+  comments,
+}) {
+  const [upvoted, setUpvoted] = useState(false)
   return (
     <>
       <div className="feedback-card">
-        <h4 className="feedback-card__title">Add tags for solutions</h4>
-        <p className="feedback-card__description">
-          Easier to search for solutions based on a specific stack.
-        </p>
-        <div className="feedback-card__category">Enhancement</div>
-        <button className="btn btn--upvote">
-          <img src="/src/assets/shared/icon-arrow-up.svg" alt="upvote icon" />
-          <span className="btn__text">99</span>
+        <h4 className="feedback-card__title">{title}</h4>
+        <p className="feedback-card__description">{description}</p>
+        <div className="feedback-card__category">{category}</div>
+        <button className={`btn btn--upvote ${upvoted ? "btn--upvoted" : ""}`}>
+          <img
+            src="/src/assets/shared/icon-arrow-up.svg"
+            alt="upvote icon"
+            className={`${upvoted ? "btn__icon--upvoted" : ""}`}
+          />
+          <span
+            className="btn__text"
+            onClick={(event) => {
+              setUpvoted((prev) => !prev)
+              console.log(upvoted)
+            }}
+          >
+            {upvotes + upvoted}
+          </span>
         </button>
         <button className="btn btn--comments">
           <img src="/src/assets/shared/icon-comments.svg" alt="comment icon" />
-          <span className="btn__text">4</span>
+          <span className="btn__text">{comments?.length}</span>
         </button>
       </div>
     </>
   )
 }
-
-// "id": 2,
-// "title": "Add a dark theme option",
-// "category": "feature",
-// "upvotes": 99,
-// "status": "suggestion",
-// "description": "It would help people with light sensitivities and who prefer dark mode.",
-// "comments": [
