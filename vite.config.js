401ffe8253dc54231aad7cb39be4fa236ddd29fd
ReversalGM <zhouhyan.steven@gmail.com>
2024-06-17 22:37:30 -0400

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "product-feedback-app/",
+  base: "/product-feedback-app/",
   plugins: [react()],
   build: {
     rollupOptions: {
